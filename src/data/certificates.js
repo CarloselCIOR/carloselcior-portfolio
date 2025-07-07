2da0534506c0863a60b5b30c22346b3bd77d9349
Carlos Ismael Orozco Rios <carloselcior@gmail.com>
2025-07-06 23:39:23 -0700

    Update 2025

---
+++ src/data/certificates.js
@@ -0,0 +1,46 @@
+// src/data/certificates.js
+
+export const certificates = [
+  {
+    title: "CCNA: Switching, Routing, and Wireless Essentials",
+    issuer: "Cisco",
+    icon: "https://media.licdn.com/dms/image/v2/D560BAQGo0LANKsqgCg/company-logo_100_100/B56ZaSzDOOGoAQ-/0/1746219578583/cisco_logo?e=1757548800&v=beta&t=ZW6_8_iqoxIUyTMw5BsBtxHq5VQMoYiey-JjI3ahWFA", 
+    url: "https://www.credly.com/badges/f747da00-c5a9-44f8-989b-32190cfced0d/linked_in_profile"
+  },
+  {
+    title: "Fundamentals of Agile Project Managment",
+    issuer: "Project Management Institute",
+    icon: "https://media.licdn.com/dms/image/v2/C4E0BAQFYYvw5asgdjQ/company-logo_100_100/company-logo_100_100/0/1630588508487/project_management_institute_logo?e=1757548800&v=beta&t=d6N8ynZ0i5QblmsiV7U2nKyFnhinTjToLfikqr354t0",
+    url: "https://www.credly.com/badges/c9585ae4-8e07-4342-9764-533033f51a24/linked_in_profile"
+  },
+    {
+    title: "Programa Oracle Next Education F2 T5 Back-end",
+    issuer: "Alura Latam",
+    icon: "https://media.licdn.com/dms/image/v2/D4D0BAQGxDpCf6Cm0xQ/company-logo_100_100/company-logo_100_100/0/1701180993504/alura_latam_logo?e=1757548800&v=beta&t=wJ8hjFYtjViYw4TjsGpOXZksBycv5GRMKUN7u3YXPv0",
+    url: "https://app.aluracursos.com/program/certificate/ed44f91b-d090-4592-8970-493df83c5b5e"
+  },
+  {
+    title: "Formación Desarrollo Personal G5 - ONE",
+    issuer: "Alura Latam",
+    icon: "https://media.licdn.com/dms/image/v2/D4D0BAQGxDpCf6Cm0xQ/company-logo_100_100/company-logo_100_100/0/1701180993504/alura_latam_logo?e=1757548800&v=beta&t=wJ8hjFYtjViYw4TjsGpOXZksBycv5GRMKUN7u3YXPv0",
+    url: "https://app.aluracursos.com/degree/certificate/47efd792-7555-447a-904e-80ccecc0064f"
+  },
+  {
+    title: "Formación Principiante en programación G5 - ONE",
+    issuer: "Alura Latam",
+    icon: "https://media.licdn.com/dms/image/v2/D4D0BAQGxDpCf6Cm0xQ/company-logo_100_100/company-logo_100_100/0/1701180993504/alura_latam_logo?e=1757548800&v=beta&t=wJ8hjFYtjViYw4TjsGpOXZksBycv5GRMKUN7u3YXPv0",
+    url: "https://app.aluracursos.com/degree/certificate/a290c253-387a-45f0-b947-bf56c7723506"
+  },
+  {
+    title: "Formación Business Agility G5 - ONE",
+    issuer: "Alura Latam",
+    icon: "https://media.licdn.com/dms/image/v2/D4D0BAQGxDpCf6Cm0xQ/company-logo_100_100/company-logo_100_100/0/1701180993504/alura_latam_logo?e=1757548800&v=beta&t=wJ8hjFYtjViYw4TjsGpOXZksBycv5GRMKUN7u3YXPv0",
+    url: "https://app.aluracursos.com/degree/certificate/a290c253-387a-45f0-b947-bf56c7723506"
+  },
+  {
+    title: "Formación Java Orientado a Objetos G5 - ONE",
+    issuer: "Alura Latam",
+    icon: "https://media.licdn.com/dms/image/v2/D4D0BAQGxDpCf6Cm0xQ/company-logo_100_100/company-logo_100_100/0/1701180993504/alura_latam_logo?e=1757548800&v=beta&t=wJ8hjFYtjViYw4TjsGpOXZksBycv5GRMKUN7u3YXPv0",
+    url: "https://app.aluracursos.com/degree/certificate/78208c24-b84e-4ff3-93b1-a4b3ae068aa2"
+  }
+];
